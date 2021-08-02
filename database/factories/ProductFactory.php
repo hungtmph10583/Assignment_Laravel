6d778d781b8247106e0fded3114151dfc92e3a50
tr.ma.h___] <84056565+hungtmph10583@users.noreply.github.com>
2021-08-02 09:15:01 +0700

    update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductFactory extends Factory
@@ -21,8 +22,16 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+        $imgName = $this->faker->image(storage_path("app/public/uploads/products"), $width = 640, $height = 480, 'cats', false);
         return [
-            //
+            'name' => $this->faker->name(),
+            'cate_id' => Category::all()->random()->id,
+            'image' => $this->faker->imageUrl(),
+            'image' => "uploads/products/" . $imgName,
+            'price' => $this->faker->numberBetween(10000, 60000),
+            'status' => rand(0, 1),
+            'quantity' => rand(10, 60),
+            'detail' => $this->faker->realText($maxNbChars = 200, $indexSize = 2)
         ];
     }
 }
